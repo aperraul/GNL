18012e101dbc8596455222ca261df13c70a68e02
Alexis PERRAULT <aperraul@e3r7p11.42.fr>
2016-02-02 15:11:03 +0100

    norme

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: aperraul <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/22 15:26:45 by aperraul          #+#    #+#             */
-/*   Updated: 2016/02/02 12:30:15 by aperraul         ###   ########.fr       */
+/*   Updated: 2016/02/02 15:10:23 by aperraul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,18 @@ static int		ft_last_line(char **text, int ret)
 	return (1);
 }
 
+static int		gnl_alloc(char **text, char **buff)
+{
+	if ((!(*buff = ft_strnew(BUFF_SIZE + 1))))
+		return (-1);
+	if (!(*text))
+	{
+		if (!(*text = ft_strnew(1)))
+			return (-1);
+	}
+	return (0);
+}
+
 int				get_next_line(const int fd, char **line)
 {
 	static char	*text = NULL;
@@ -63,13 +75,15 @@ int				get_next_line(const int fd, char **line)
 	char		*temp;
 	int			ret;
 
-	if ((ret = 1) && (!(buff = ft_strnew(BUFF_SIZE + 1))))
+	if ((ret = 1) && gnl_alloc(&text, &buff) == -1)
 		return (-1);
-	if (!(text))
-	{
-		if (!(text = ft_strnew(1)))
-			return (-1);
-	}
+//	if ((ret = 1) && (!(buff = ft_strnew(BUFF_SIZE + 1))))
+//		return (-1);
+//	if (!(text))
+//	{
+//		if (!(text = ft_strnew(1)))
+//			return (-1);
+//	}
 	while (!(ft_strchr(text, '\n')) && ret > 0)
 	{
 		if ((ret = read(fd, buff, BUFF_SIZE)) == -1)
